7fcfe0f617897047b3547f4c62f13f1fb78d6ab3
Milan Pattni <milan@pattni.ca>
2024-09-09 23:15:19 -0400

    navbar logic and cc

--- src/components/assets/navbar/Navbar.tsx
+++ src/components/assets/navbar/Navbar.tsx
@@ -1,36 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Importing icons
 import Signature from '../../../images/signature.png'; // Assuming this is the path to your signature image
 import './Navbar.css';
 
 const Navbar: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <nav>
-            <Link to="/" className="brand">
-                <img
-                    src={Signature}
-                    alt="Milan Pattni Signature"
-                    className="signature"
-                />
-            </Link>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/experiences">Experiences</Link>
-                </li>
-                <li>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            {/* Dark overlay that appears when the menu is open */}
+            <div
+                className={menuOpen ? 'menu-overlay active' : 'menu-overlay'}
+                onClick={toggleMenu}
+            ></div>
+
+            <nav>
+                <div className="nav-content">
+                    <Link to="/" className="brand">
+                        <img
+                            src={Signature}
+                            alt="Milan Pattni Signature"
+                            className="signature"
+                        />
+                    </Link>
+                    {/* Hamburger icon - toggle between open and close */}
+                    <div className="hamburger" onClick={toggleMenu}>
+                        {menuOpen ? <FaTimes /> : <FaBars />}
+                    </div>
+                    <ul className={menuOpen ? 'active' : ''}>
+                        <li>
+                            <Link to="/" onClick={toggleMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/experiences" onClick={toggleMenu}>
+                                Experiences
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/projects" onClick={toggleMenu}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" onClick={toggleMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
     );
 };
 
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,33 +1,49 @@
 import React from 'react';
 import { IoLogoLinkedin, IoIosDocument } from 'react-icons/io';
 import './Home.css'; // Import the CSS file
+import CourseClutch from '../../images/courseclutch.png';
 
 const Home: React.FC = () => {
     return (
-        <div className="home-container">
-            <p style={{ marginBottom: '5px' }}>👋 Hey! I'm</p>
-            <h1>Milan Pattni</h1>
-            <p>
-                a Mechatronics Engineering student at the University of
-                Waterloo.
-            </p>
-            <div className="button-container">
-                <a
-                    href="https://www.linkedin.com/in/pattni"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn linkedin-btn"
-                >
-                    <IoLogoLinkedin /> LinkedIn
-                </a>
-                <a
-                    href="https://drive.google.com/file/d/1PbCPMxl7v5ztaInkgy_vCxyGc2hIEsrI/view?usp=drive_link"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn resume-btn"
-                >
-                    <IoIosDocument /> Resume
-                </a>
+        <div className="container">
+            <div className="home-container">
+                <p style={{ marginBottom: '5px' }}>👋 Hey! I'm</p>
+                <h1>Milan Pattni</h1>
+                <p>
+                    I Study Mechatronics Engineering at the University of
+                    Waterloo.
+                </p>
+                <div className="button-container">
+                    <a
+                        href="https://www.linkedin.com/in/pattni"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn linkedin-btn"
+                    >
+                        <IoLogoLinkedin /> LinkedIn
+                    </a>
+                    <a
+                        href="https://drive.google.com/file/d/1PbCPMxl7v5ztaInkgy_vCxyGc2hIEsrI/view?usp=drive_link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn resume-btn"
+                    >
+                        <IoIosDocument /> Resume
+                    </a>
+                    <a
+                        href="https://www.courseclutch.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn cc-btn"
+                    >
+                        <img
+                            src={CourseClutch}
+                            alt="Course Clutch Logo"
+                            className="cc-image"
+                        />
+                        Course Clutch
+                    </a>
+                </div>
             </div>
         </div>
     );
