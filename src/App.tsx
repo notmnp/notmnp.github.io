4d55da174e9930194ec546bcbbcbbc86d7b16ced
notmnp <milan@pattni.ca>
2025-02-09 19:59:27 -0500

    Switched to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Navbar from './components/assets/navbar/Navbar';
 import Footer from './components/assets/footer/Footer';
@@ -7,7 +7,7 @@ import Connect4 from './components/assets/connect4/Connect4';
 
 function App() {
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
